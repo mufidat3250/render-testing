007aeaaca5677d7edaf3e9ef2feba7561cfcb65a
mufidat3250 <wahabmufidat1919@gmail.com>
2023-03-01 15:59:28 +0100

    wip:get request

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+let persons = [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456",
+    },
+    {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+    },
+    {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345",
+    },
+    {
+      id: 4,
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+    },
+  ];
 
 app.get('/', (request, response)=>{
-    response.send(`<h3>i am ok</h3>`)
+    response.json(persons)
 })
 
 
